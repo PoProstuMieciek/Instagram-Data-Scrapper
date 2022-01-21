26bae270cd0644834d91638be342381a8cc19ba6
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-21 23:42:52 +0100

    feat: Add example code to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,28 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
+
+import { readFileSync } from 'fs';
+import { homedir } from 'os';
+
+import { ObjectStorageProvider } from './providers/ObjectStorageProvider';
 import { User } from './entities/User.entity';
 
 createConnection().then(async (connection) => {
+    const objectStorage = new ObjectStorageProvider();
     const userRepo = connection.getRepository(User);
 
     const user = new User();
     user.firstName = 'Jan';
     user.lastName = 'Kowalski';
-    user.age = '20';
+    user.age = 20;
+
+    const { id } = await userRepo.save(user);
+
+    const profilePicture = await objectStorage.upload(
+        `profile_picture_${id}.png`,
+        readFileSync(`${homedir()}/Pictures/profile_picture.png`)
+    );
+    user.profilePictureETag = profilePicture.ETag;
 
     await userRepo.save(user);
 });

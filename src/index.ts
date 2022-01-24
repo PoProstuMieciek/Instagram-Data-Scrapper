8cb0ba0eb5c5a9c31cefad25a71caf887039ca37
Maciej Opaliński <poprostumieciek@gmail.com>
2022-01-24 11:50:06 +0100

    fix: Add example logging to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './config';
+
 import { createConnection } from 'typeorm';
 
 import { readFileSync } from 'fs';
@@ -6,9 +8,19 @@ import { homedir } from 'os';
 import { ObjectStorageProvider } from './providers/ObjectStorageProvider';
 import { User } from './entities/User.entity';
 
+import logger from './utils/ConsoleLogger';
+
+logger.log('debug', '127.0.0.1 - there is no place like home');
+logger.log('info', '127.0.0.1 - there is no place like home');
+logger.log('notice', '127.0.0.1 - there is no place like home');
+logger.log('warn', '127.0.0.1 - there is no place like home');
+logger.log('error', '127.0.0.1 - there is no place like home');
+logger.log('crit', '127.0.0.1 - there is no place like home');
+logger.log('alert', '127.0.0.1 - there is no place like home');
+logger.log('emerg', '127.0.0.1 - there is no place like home');
+
 createConnection().then(async (connection) => {
     const objectStorage = new ObjectStorageProvider();
-
     const userRepo = connection.getRepository(User);
 
     const user = new User();

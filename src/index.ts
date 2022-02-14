f4dc69557e6f640d5ef4ae6b85d2477b5cdf368d
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-14 13:45:54 +0100

    fix: Remove example code from `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
 import { createConnection } from 'typeorm';
-
-import { Subpage } from './entities';
+import Logger from './utils/ConsoleLogger';
 
 import './config';
 
-createConnection().then(async (connection) => {
-    const subpagesRepo = connection.getRepository(Subpage);
-    const subpage = new Subpage();
-    subpage.html =
-        '<html><head><title>Subpage</title></head><body><p>Hello, world!</p></body></html>';
-    subpage.path = 'http://localhost:3000/hello';
-    await subpagesRepo.save(subpage);
+createConnection().then(async () => {
+    Logger.info('Connected successfully!');
 });

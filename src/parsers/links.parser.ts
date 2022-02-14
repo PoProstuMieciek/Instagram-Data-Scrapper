e6def56b826f6d71dd91056ffcf616b767041e57
WojtekWidomski <98206524+WojtekWidomski@users.noreply.github.com>
2022-02-14 15:08:18 +0100

    fix: Fix file formatting

--- src/parsers/links.parser.ts
+++ src/parsers/links.parser.ts
@@ -1,10 +1,9 @@
 import { JSDOM } from 'jsdom';
 
 export const parseLinks = (dom: JSDOM) => {
-    const links: string[] = [];
     const document = dom.window.document;
-    const anchors = document.querySelectorAll("a");
+    const anchors = document.querySelectorAll('a');
     const anchors_arr = Array.from(anchors);
-    const href_arr = anchors_arr.map((a) => a.href)
+    const href_arr = anchors_arr.map((a) => a.href);
     return href_arr;
 };

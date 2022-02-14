93c66bc59b5f5d350463e77913c4d5ff2809eb13
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-14 13:45:19 +0100

    feat: Add `parseSubpage` function

---
+++ src/parsers/Subpage.parser.ts
@@ -0,0 +1,33 @@
+import { getRepository } from 'typeorm';
+import axios from 'axios';
+import { Subpage } from '../entities';
+
+export const parseSubpage = async (url: string, current_depth = 1) => {
+    const subpagesRepo = getRepository(Subpage);
+
+    const found = await subpagesRepo.findOne({ where: { path: url } });
+    if (found) return;
+
+    const { data } = await axios.get<string>(url);
+
+    const subpage = new Subpage();
+    subpage.path = url;
+    subpage.html = data;
+
+    // TODO: call `HTMLParser` #53
+
+    // TODO: call `LinksParser` #54
+    subpage.referencedLinks = [];
+
+    // TODO: call `WordsParser` #55
+    subpage.statistics = [];
+
+    // TODO: call `ImagesParser` #56
+    subpage.images = [];
+
+    subpagesRepo.save(subpage);
+
+    subpage.referencedLinks.forEach((link) => {
+        parseSubpage(link.path, current_depth + 1);
+    });
+};

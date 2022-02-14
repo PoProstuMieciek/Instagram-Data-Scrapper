7655d028546ee8f2124c8a53972b883f9dff271b
WojtekWidomski <98206524+WojtekWidomski@users.noreply.github.com>
2022-02-14 14:52:38 +0100

    fix: Use const

--- src/parsers/links.parser.ts
+++ src/parsers/links.parser.ts
@@ -1,9 +1,9 @@
 import { JSDOM } from 'jsdom';
 
 export const parseLinks = (dom: JSDOM) => {
-    let links: string[] = [];
+    const links: string[] = [];
     const document = dom.window.document;
-    let anchors = document.querySelectorAll("a");
+    const anchors = document.querySelectorAll("a");
     anchors.forEach(
         function(value, idx, list){
             links.push(value.href);

4a6d9c117072da9e50530420c136428cbd3ec349
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-14 14:26:27 +0100

    feat: Add example usage to `index.ts`

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import { createConnection } from 'typeorm';
-import Logger from './utils/ConsoleLogger';
+import { parseSubpage } from './parsers';
 
+import Logger from './utils/ConsoleLogger';
 import './config';
 
 createConnection().then(async () => {
     Logger.info('Connected successfully!');
+
+    parseSubpage('https://en.wikipedia.org/wiki/Comedy_film');
 });

14f7e1f5284de0a9889d7f2c16a59ac8abf89d5e
Maciej Opaliński <poprostumieciek@gmail.com>
2022-02-16 11:41:20 +0100

    fix: Fix parsers and object store

--- src/parsers/images.parser.ts
+++
@@ -1,9 +0,0 @@
-import { JSDOM } from 'jsdom';
-
-export const parseImages = (jsdom: JSDOM) => {
-    const images = jsdom.window.document.querySelectorAll('img');
-    const images_arr = Array.from(images);
-
-    const href_arr = images_arr.map((img) => img.src);
-    return href_arr;
-};
